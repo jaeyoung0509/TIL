0f1b73c75d774f8d0087348974b70e0eaf729e29
jaeyoung0509 <jaeyoung0509@naver.com>
2022-11-18 15:15:52 +0900

    design pattern

---
+++ language/python/python-clean-code/composite.py
@@ -0,0 +1,20 @@
+from collections.abc import Iterable
+class Product:
+    def __init__(self, name, price):
+        self._name = name 
+        self._price = price 
+
+    @property
+    def price(self):
+        return self._price
+
+class ProductBundle:
+    def __init__(self,name,prec_discount,*products: Iterable[Product | "ProductBundle"]):
+        self._name = name
+        self._perc_discount = prec_discount
+        self._products = products
+    
+    @property
+    def price(self):
+        total = sum(p.price for p in self._products)
+        return total * (1 - self._perc_discount)
