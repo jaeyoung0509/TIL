f4fbdeb513a0788248b07ecc694298149b7f629a
jaeyoung0509 <jaeyoung0509@naver.com>
2022-11-18 14:29:28 +0900

    design pattern

---
+++ language/python/python-clean-code/design_pattern01.py
@@ -0,0 +1,19 @@
+class GitFetcher:
+    _current_tag = None
+
+    def __init__(self,tag):
+        self._current_tag = tag
+
+    @property
+    def current_tag(self):
+        if self._current_name is None:
+            raise AttributeError("tag is not initialized")
+        return self._current_tag   
+
+    
+    @current_tag.setter
+    def current_tag(self,new_tag):
+        self.__class__.current_tag = new_tag
+
+    def pull(self):
+        return self.current_tag
---
+++ language/python/python-clean-code/generator.py
@@ -0,0 +1,11 @@
+def sequence(name , start , end):
+    yield from range(start, end)
+    return end
+
+def main():
+    step = yield from sequence("first" , 0, 5)
+    step2 = yield from sequence("second" , step, 5)
+    return step + step2
+
+
+g = main()
